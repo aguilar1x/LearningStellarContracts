becc05cbe7bf7e2327ea30a9bf30a0e0036c104d
aguilar1x <aaguilar1x@gmail.com>
2025-04-09 23:57:59 -0600

    Init Repo

---
+++ LearningContracts/rating-system-contract/src/history.rs
@@ -0,0 +1,21 @@
+use crate::DataKey;
+use crate::{datatypes::Error, rating::Rating, reputation::ReputationRecord};
+use soroban_sdk::{Address, Env, Vec};
+
+// Retrieves the rating history for a given seller
+pub fn _get_rating_history(env: Env, seller: &Address) -> Result<Vec<Rating>, Error> {
+    let key = DataKey::RatingHistory(seller.clone());
+    match env.storage().instance().get(&key) {
+        Some(ratings) => Ok(ratings),
+        None => Err(Error::RatingHistoryNotFound),
+    }
+}
+
+// Retrieves the reputation history for a given seller
+pub fn get_reputation_history(env: Env, seller: Address) -> Result<Vec<ReputationRecord>, Error> {
+    let key = DataKey::ReputationHistory(seller.clone());
+    match env.storage().instance().get(&key) {
+        Some(reputation_record) => Ok(reputation_record),
+        None => Err(Error::ReputationHistoryNotFound),
+    }
+}
